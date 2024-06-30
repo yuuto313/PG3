6ce82eb9c6a01271983a1e04714d47e108d40461
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-06-30 13:33:19 +0900

    セーブ

---
+++ Input.cpp
@@ -0,0 +1,11 @@
+#include "Input.h"
+
+void Input::Initialize()
+{
+	//DirectInputのインスタンス生成
+	ComPtr<IDirectInput8> directInput
+}
+
+void Input::Update()
+{
+}
---
+++ Input.h
@@ -0,0 +1,19 @@
+#pragma once
+/// <summary>
+/// 入力
+/// </summary>
+class Input
+{
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+};
+
