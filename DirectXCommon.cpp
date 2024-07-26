0bc2920427bca4855da562e068aaf93257a9a227
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-07-26 14:03:38 +0900

    セーブ

--- DirectXCommon.cpp
+++ DirectXCommon.cpp
@@ -1,5 +1,19 @@
 #include "DirectXCommon.h"
+#include <cassert>
+
+#include "Logger.h"
+#include "StringUtilty.h"
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+using namespace Microsoft::WRL;
 
 void DirectXCommon::Initialize()
 {
+	HRESULT result;
+
+	//デバイス生成
+
+
 }
--- DirectXCommon.h
+++ DirectXCommon.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl.h>
 
 /// <summary>
 /// DIrectX基盤
@@ -11,5 +14,10 @@ public:
 	/// 初期化
 	/// </summary>
 	void Initialize();
+
+private:
+	//DirectX12デバイス
+	Microsoft::WRL::ComPtr<ID3D12Device> device;
+	Microsoft::WRL::ComPtr<IDXGIFactory> dxgiFactory;
 };
 
