3252fcdf2a75cbc274b743fee52876f1d699c70d
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-09-30 11:46:33 +0900

    セーブ

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <Windows.h>
 
+template<typename Type1,typename Type2,typename Type3,typename Type4>
+
+Type1 Min(Type1 a, Type2 b,Type3 c,Type4 d) {
+	
+}
+
 int main() {
 	// UTF-8を設定
 	SetConsoleOutputCP(65001);
