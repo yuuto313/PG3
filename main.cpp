00f2dc386059d8b817c62cbc264a86a7cd018a1a
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-09-30 12:35:56 +0900

    01_01課題

--- main.cpp
+++ main.cpp
@@ -1,17 +1,51 @@
+#include <stdio.h>
 #include <iostream>
 #include <Windows.h>
 
-template<typename Type1,typename Type2,typename Type3,typename Type4>
+template<typename Type>
 
-Type1 Min(Type1 a, Type2 b,Type3 c,Type4 d) {
-	
+Type Min(Type a, Type b) {
+	if (a < b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+template<>
+
+char Min(char a, char b) {
+	printf("数字以外は代入できません！\n");
+	return 0;
 }
 
 int main() {
-	// UTF-8を設定
-	SetConsoleOutputCP(65001);
+	// int型
+	int x = 3;
+	int y = 5;
 
-	printf("小澤ダヨ");
+	// float型
+	float a = 90.0f;
+	float b = 25.0f;
+
+	// double型
+	double p = 3.141592;
+	double q = 4.252603;
+
+	char str1 = 'K';
+	char str2 = 'G';
+
+	printf("intの比較 : %d,%d\n",x,y);
+	printf("%d\n", Min(x,y));
+
+	printf("floatの比較 : %f,%f\n",a,b);
+	printf("%f\n", Min(a,b));
+
+	printf("doubleの比較 : %f,%f\n",p,q);
+	printf("%f\n", Min(p,q));
+
+	printf("charの比較 : %c,%c\n",str1,str2);
+	Min(str1, str2);
 
 	return 0;
 }
