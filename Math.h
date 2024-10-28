7d4c0561f4ce25a2d3720479078b8884d1878e8d
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-10-28 12:17:09 +0900

    セーブ

--- Math.h
+++ Math.h
@@ -10,13 +10,19 @@ public:
 	Type2 b;
 
 	// コンストラクタ
-	Math(Type1 _a, Type2 _b) : a(a), b(b) {};
+	Math(Type1 a, Type2 b) : a(a), b(b) {};
 
 	// メンバ関数
 	
 	/// <summary>
 	/// 2つの引数の値を比べて小さい値を返す
 	/// </summary>
-	Type1 Min(Type1 _a,Type2 _b);
+	auto Min(Type1 _a, Type2 _b) {
+		if (_a < _b) {
+			return _a;
+		} else {
+			return _b;
+		}
+	}
 
 };
--- main.cpp
+++ main.cpp
@@ -4,18 +4,27 @@
 #include <time.h>
 #include <functional>
 
-#include "Enemy.h"
-
-#define FREQUENCY 3
+#include "Math.h"
 
 int main(int argc,char* argv[]) {
+	// int型の比較
+	Math<int,int> b1(10, 20);
 
-	Enemy* pEnemy = nullptr;
-	pEnemy = new Enemy();
+	printf("小さい方の値を表示します\n");
+	printf("%dと%dの比較結果 : %d\n",b1.a,b1.b, b1.Min(b1.a, b1.b));
 
-	for (int i = 0; i < FREQUENCY; i++) {
-		pEnemy->Execute();
-	}
+	// float型の比較
+	Math<float,float> b2(30.0f, 20.0f);
+	printf("%fと%fの比較結果 : %f\n", b2.a, b2.b, b2.Min(b2.a, b2.b));
+
+	// double型の比較
+	Math<double,double> b3(20.0, 5.0);
+	printf("%lfと%lfの比較結果 : %lf\n", b3.a, b3.b, b3.Min(b3.a, b3.b));
+
+	// intとfloat型の比較
+	Math<int, float> b4(40, 39.0f);
+	printf("%dと%fの比較結果 : %d\n", b4.a, b4.b, b4.Min(b4.a, b4.b));
+
 
 	return 0;
 }
