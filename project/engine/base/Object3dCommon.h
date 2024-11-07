85b4d7970f935781c451a6563e56ce6340528995
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-11-07 15:22:09 +0900

    セーブ

--- project/engine/base/Object3dCommon.h
+++ project/engine/base/Object3dCommon.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "DirectXCommon.h"
 
 /// <summary>
 /// 3Dオブジェクト共通部
@@ -7,9 +8,65 @@ class Object3dCommon
 {
 public:
 
+	//-------------基本処理-------------//
+
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize();
+	/// <param name="dxCommon"></param>
+	void Initialize(DirectXCommon* dxCommon);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void ImGui();
+
+	/// <summary>
+	/// 共通描画設定
+	/// </summary>
+	void SetDraw();
+
+	//-------------ゲッター・セッター-------------//
+
+	DirectXCommon* GetDxCommon()const { return dxCommon_; }
+
+private:
+
+	//-------------メンバ変数-------------//
+
+	DirectXCommon* dxCommon_;
+
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature_;
+
+	D3D12_DESCRIPTOR_RANGE descriptorRange_[1] = {};
+	D3D12_ROOT_PARAMETER rootParameters_[4] = {};
+	D3D12_STATIC_SAMPLER_DESC staticSamplers_[1] = {};
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc_{};
+	Microsoft::WRL::ComPtr<IDxcBlob> vertexShaderBlob_;
+	Microsoft::WRL::ComPtr<IDxcBlob> pixelShaderBlob_;
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs_[3] = {};
+	D3D12_BLEND_DESC blendDesc_{};
+	D3D12_RASTERIZER_DESC rasterizerDesc_{};
+	D3D12_DEPTH_STENCIL_DESC depthStencilDesc_{};
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_;
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc_{};
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState_;
+
+	//-------------メンバ関数-------------//
+
+	/// <summary>
+	/// ルートシグネイチャの作成
+	/// </summary>
+	void CreateRootSignature();
+
+	/// <summary>
+	/// グラフィックスパイプラインの生成
+	/// </summary>
+	void CreateGraphicsPipeline();
+
+	static Microsoft::WRL::ComPtr<ID3D12PipelineState> CreateGraphicsPipelineState(Microsoft::WRL::ComPtr<ID3D12Device> device, const D3D12_GRAPHICS_PIPELINE_STATE_DESC& desc);
 };
 
