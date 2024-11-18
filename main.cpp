a6ecbce35d1ef040a093350f139276a4c7a65a63
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-11-18 10:54:59 +0900

    セーブ

--- main.cpp
+++ main.cpp
@@ -2,22 +2,18 @@
 #include <Windows.h>
 #include <time.h>
 #include <functional>
-#include <vector>
+#include <list>
+#include <iostream>
 
-#include "Circle.h"
-#include "Rect.h"
+using namespace std;
 
 int main(int argc,char* argv[]) {	
 
-	std::vector<std::unique_ptr<IShape>> shapes;
+	list<char> lst{ "Tokyo","Kanda" };
 
-	shapes.push_back(std::make_unique<Circle>());
-	shapes.push_back(std::make_unique<Rect>());
 
-	for (auto& shape : shapes) {
-		shape->Info();
-		shape->Size();
-		shape->Draw();
+	for (list<char>::iterator itr = lst.begin(); itr != lst.end(); ++itr) {
+		cout << *itr << "\n";
 	}
 
 	return 0;
