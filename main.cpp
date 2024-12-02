5185b98ce6d7a608b66d9d92755e5432cec5e64d
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-12-02 16:16:28 +0900

    06_02課題

--- main.cpp
+++ main.cpp
@@ -1,56 +1,62 @@
 #include <stdio.h>
-#include <Windows.h>
-#include <time.h>
-#include <functional>
-#include <vector>
+#include <thread>
 #include <iostream>
-#include <cstring>
-#include <algorithm>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
+int num = 1;
+mutex mtx;              // 手動でロック (lock()) とアンロック (unlock()) を行う必要がある
+condition_variable cv;  // スレッドが特定の条件を満たすまで待機するためのツール
+// notify_all()         //条件変数を使用して待機しているすべてのスレッドを再開させるための関数 
+
+
+void PrintThread1() {
+	unique_lock<mutex> lock(mtx);
+	while (num != 1) {
+		cv.wait(lock);  // numが1になるまで待つ
+	}
+
+	cout << "thread1" << endl;
+
+	num = 2;  // 次に実行するスレッドを2に設定
+	cv.notify_all();  // 他のスレッドに通知
+}
+
+void PrintThread3() {
+	unique_lock<mutex> lock(mtx);
+	while (num != 3) {
+		cv.wait(lock);  // numが3になるまで待つ
+	}
+
+	cout << "thread3" << endl;
+
+	num = 3;  // 次に実行するスレッドを設定
+	cv.notify_all();
+}
+
+void PrintThread2() {
+	unique_lock<mutex> lock(mtx);
+	while (num != 2) {
+		cv.wait(lock);  // numが2になるまで待つ
+	}
+
+	cout << "thread2" << endl;
+
+	num = 3;  // 次に実行するスレッドを3に設定
+	cv.notify_all();
+}
+
 int main(int argc,char* argv[]) {	
 
-	vector<string> lst = { 
-		"k022g0108@g.neec.ac.jp","k022g0045@g.neec.ac.jp","k022g0007@g.neec.ac.jp","k022g0015@g.neec.ac.jp",
-		"k022g0028@g.neec.ac.jp","k022g0049@g.neec.ac.jp","k022g0003@g.neec.ac.jp","k022g0044@g.neec.ac.jp",
-		"k022g0072@g.neec.ac.jp","k022g0055@g.neec.ac.jp","k021g1139@g.neec.ac.jp","k021g1485@g.neec.ac.jp",
-		"k021g1162@g.neec.ac.jp","k021g1278@g.neec.ac.jp","k020g1229@g.neec.ac.jp","k020g1202@g.neec.ac.jp",
-		"k020g1215@g.neec.ac.jp","k020g1416@g.neec.ac.jp","k020g1245@g.neec.ac.jp","k020g1506@g.neec.ac.jp",
-		"k020g1401@g.neec.ac.jp","k022g0089@g.neec.ac.jp","k022g0107@g.neec.ac.jp","k022g0135@g.neec.ac.jp",
-		"k022g0020@g.neec.ac.jp","k022g0046@g.neec.ac.jp","k022g0126@g.neec.ac.jp","k022g0068@g.neec.ac.jp",
-		"k022g1060@g.neec.ac.jp","k022g0019@g.neec.ac.jp","k022g0013@g.neec.ac.jp","k022g0024@g.neec.ac.jp",
-		"k022g0134@g.neec.ac.jp","k022g0123@g.neec.ac.jp","k022g0030@g.neec.ac.jp","k022g0053@g.neec.ac.jp",
-		"k022g0023@g.neec.ac.jp","k022g0041@g.neec.ac.jp","k022g0063@g.neec.ac.jp","k022g0059@g.neec.ac.jp",
-		"k022g0078@g.neec.ac.jp","k022g0011@g.neec.ac.jp","k022g0040@g.neec.ac.jp","k022g0088@g.neec.ac.jp",
-		"k022g0099@g.neec.ac.jp","k022g0060@g.neec.ac.jp","k022g0032@g.neec.ac.jp","k022g0087@g.neec.ac.jp",
-		"k022g0016@g.neec.ac.jp","k022g0116@g.neec.ac.jp","k022g0056@g.neec.ac.jp","k022g0009@g.neec.ac.jp",
-		"k022g0034@g.neec.ac.jp","k022g0062@g.neec.ac.jp","k022g0102@g.neec.ac.jp","k022g0113@g.neec.ac.jp",
-		"k022g0064@g.neec.ac.jp","k022g0080@g.neec.ac.jp","k022g0114@g.neec.ac.jp","k022g0025@g.neec.ac.jp",
-		"k022g0095@g.neec.ac.jp","k022g0121@g.neec.ac.jp","k022g0021@g.neec.ac.jp","k022g0104@g.neec.ac.jp",
-		"k022g0101@g.neec.ac.jp","k022g0130@g.neec.ac.jp","k022g0036@g.neec.ac.jp","k022g0004@g.neec.ac.jp",
-		"k022g0093@g.neec.ac.jp","k022g0038@g.neec.ac.jp","k022g0069@g.neec.ac.jp","k022g0029@g.neec.ac.jp",
-		"k022g0010@g.neec.ac.jp","k022g0083@g.neec.ac.jp","k022g0086@g.neec.ac.jp","k022g0129@g.neec.ac.jp",
-		"k022g0048@g.neec.ac.jp","k022g0027@g.neec.ac.jp","k022g0103@g.neec.ac.jp","k022g0117@g.neec.ac.jp",
-		"k022g0085@g.neec.ac.jp","k022g0052@g.neec.ac.jp","k022g0073@g.neec.ac.jp","k022g0065@g.neec.ac.jp",
-		"k022g0091@g.neec.ac.jp","k022g0136@g.neec.ac.jp","k022g0079@g.neec.ac.jp","k022g0017@g.neec.ac.jp",
-		"k022g0097@g.neec.ac.jp","k022g0077@g.neec.ac.jp","k022g0050@g.neec.ac.jp","k022g0033@g.neec.ac.jp",
-		"k022g0039@g.neec.ac.jp","k022g0118@g.neec.ac.jp","k022g0112@g.neec.ac.jp","k022g0120@g.neec.ac.jp",
-		"k022g0127@g.neec.ac.jp","k021g1464@g.neec.ac.jp","k022g0137@g.neec.ac.jp","k022g0066@g.neec.ac.jp",
-		"k022g0051@g.neec.ac.jp","k022g0111@g.neec.ac.jp","k022g0094@g.neec.ac.jp","k022g0128@g.neec.ac.jp",
-		"k022g0109@g.neec.ac.jp","k022g0124@g.neec.ac.jp","k022g0084@g.neec.ac.jp","k022g0082@g.neec.ac.jp",
-		"k022g0122@g.neec.ac.jp","k022g0061@g.neec.ac.jp","k022g0031@g.neec.ac.jp","k022g0076@g.neec.ac.jp",
-		"k022g0002@g.neec.ac.jp","k022g0090@g.neec.ac.jp","k022g0074@g.neec.ac.jp","k022g0012@g.neec.ac.jp",
-		"k022g0001@g.neec.ac.jp" };
+	thread th1(PrintThread1);
+	thread th2(PrintThread2);
+	thread th3(PrintThread3);
 
-	
-	// 昇順に並び替え
-	sort(lst.begin(), lst.end());
-
-	cout << "学籍番号順に並び替えします。" << endl;
-	for (int i = 0; i < lst.size(); ++i) {
-		cout << lst[i] << endl;
-	}
+	th1.join();
+	th2.join();
+	th3.join();
 
 
 	return 0;
